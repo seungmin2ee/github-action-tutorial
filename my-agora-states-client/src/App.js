ee60e64e572cea83240a9cedf1247845e7db1286
seungmin2ee <tmdals2815@gmail.com>
2022-09-27 18:44:24 +0900

    webpack-tutorial 완료

--- my-agora-states-client/src/App.js
+++ my-agora-states-client/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Form, Discussions } from "./components";
 import { useEffect, useState } from "react";
 
--- my-agora-states-client/webpack.config.js
+++ my-agora-states-client/webpack.config.js
@@ -1,41 +1,35 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "docs"),
     filename: "app.bundle.js",
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
         exclude: /node_modules/,
       },
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         use: {
             loader: "babel-loader",
             options: {
-                "presets": [
+                presets: [
                     "@babel/preset-env",
-                    "@babel/preset-react"
+                    ["@babel/preset-react", {runtime: 'automatic'}]
                 ]
             }
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: {
-              minimize: true,
-            },
-          },
-        ],
+        exclude: /node_modules;/,
       },
     ],
   },
@@ -43,10 +37,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
+  optimization: {
+    minimizer: [
+        new CssMinimizerPlugin(),
+    ],
+  },
   devServer: {
     host: 'localhost',
-    port: 3001,
+    port: 3000,
     open: true
   }
 };
